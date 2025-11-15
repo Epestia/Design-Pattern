e9f9b09a5acc69b25460b101575dd76cb47b1731
Epestia <epestia.lumis@hotmail.com>
2025-11-15 01:18:12 +0100

    first commit

---
+++ 01-Singleton/MemoryCache.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Singleton
+{
+    public sealed class MemoryCache
+    {
+        private Dictionary<string, object> _cacheObjects;
+        public static Lazy<MemoryCache> _cache { get; } = new (() => new());
+        public static MemoryCache Instance  => _cache.Value;
+        private MemoryCache() {
+            _cacheObjects = new();
+        }
+        public void Add(string key, object value) => _cacheObjects[key] = value;
+
+        public object? Get(string Key)
+        {
+           if(_cacheObjects.ContainsKey(Key)) return _cacheObjects[Key];
+            return null;
+        }
+    }
+}
---
+++ 01-Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using _01_Singleton;
+
+Console.WriteLine("Hello, World!");
+
+var cache = MemoryCache.Instance;
+var cache2 = MemoryCache.Instance;
+
+cache.Add("test", DateTime.Now);
+Console.WriteLine(cache2.Get("test"));
+
+Console.WriteLine(ReferenceEquals(cache, cache2)); 
+
